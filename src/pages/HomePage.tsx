b52f072181bc321d89b547fcb7b78f8abb81c4be
Nina Bobyleva <2847171@mail.ru>
2024-08-25 21:28:53 +0300

    Добавила Stylelint и Eslint, пока с ошибками.

--- src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import Wrapper from "../components/Wrapper/Wrapper";
-
-export function HomePage() {
-    return (
-        <Wrapper>
-            <h1 className="font-bold">Home page</h1>
-        </Wrapper>
-    );
-}
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import Wrapper from "../../components/Wrapper/Wrapper";
+
+
+export function HomePage() {
+  return (
+    <Wrapper>
+      <h1 className="font-bold italic">Home page</h1>
+    </Wrapper>
+  );
+}
