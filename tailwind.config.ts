ffc5909f6182ab0800e4b57133d2ffb082215eb6
Nina Bobyleva <2847171@mail.ru>
2024-08-28 01:13:33 +0300

    Верстка страницы курса

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,34 @@ export default {
     extend: {
       screens: {
         main: '1440px',
-      }
+      },
+      fontFamily: {
+        'roboto-400': 'var(--font-roboto-400)',
+        'roboto-500': 'var(--font-roboto-500)',
+      },
+      colors: {
+        background: "#FAFAFA",
+        black: "#000000",
+        white: "#FFFFFF",
+        red: "#DB0030",
+        lime: "#BCEC30",
+        limeHover: "#C6FF00",
+        yellow: "#FFC700",
+        orange: "#F7A012",
+        salmon: "#FF7E65",
+        purple: "#7D458C",
+        blueDark: "#2491D2",
+        blueLight: "#00C1FF",
+        gray: "#D0CECE",
+        grayLight: "#F7F7F7",
+        selectionBorder: "#C4C4C4",
+      },
+      cursor: {
+        'custom': 'url(/img/customCursor.svg), pointer',
+      },
+      boxShadow: {
+        def: '0 4px 67px -12px rgba(0, 0, 0, 0.13)',
+      },
     },
   },
   plugins: [],
