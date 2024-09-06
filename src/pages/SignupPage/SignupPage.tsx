57976c73f3d3fcda6f43830a66b71bd3ef86cb54
Kholov Rustam <rustamkholow5@gmail.com>
2024-09-06 13:00:54 +0300

    Firebase: Error (auth/email-already-in-use).

--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -1,22 +1,39 @@
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button/Button";
 import ButtonLink from "../../components/ButtonLink/ButtonLink";
 import InputForm from "../../components/InputForm/InputForm";
 import { ModalLogo } from "../../components/ModalLogo/ModalLogo";
 import { ModalWrapper } from "../../components/ModalWrapper/ModalWrapper";
 import { path } from "../../paths";
+import { createUser } from "../../api/authApi";
 
 export function SignupPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [repeatPass, setReapeatPass] = useState("")
+  const onChangeInput = (e:React.ChangeEvent<HTMLInputElement>, setState: (state:string) => void) => {
+    setState(e.target.value)
+  }
+  const handlerRegister = async () => {
+    try {
+      await createUser({email, password, name: email})
+    } catch (error) {
+      if(error instanceof Error){
+        console.log(error.message)
+      }
+    }
+  }
   return (
     <ModalWrapper>
       <ModalLogo>
         <div className="mb-[34px]">
-          <InputForm placeholder="Эл. почта" />
-          <InputForm placeholder="Пароль" />
-          <InputForm placeholder="Повторить пароль" />
+          <InputForm value={email} onChange={(e)=> onChangeInput(e, setEmail)} placeholder="Эл. почта" />
+          <InputForm value={password} onChange={(e)=> onChangeInput(e, setPassword)} placeholder="Пароль" />
+          <InputForm value={repeatPass} onChange={(e)=> onChangeInput(e, setReapeatPass)} placeholder="Повторить пароль" />
         </div>
 
         <div className="space-y-2.5">
-          <Button title="Зарегистрироваться" />
+          <Button onClick={handlerRegister} title="Зарегистрироваться" />
           <ButtonLink link={path.LOGIN} title="Войти" />
         </div>
       </ModalLogo>
