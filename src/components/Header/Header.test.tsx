20fe220290247e2968b9b7316fb48d6337e14b9d
Nina Bobyleva <2847171@mail.ru>
2024-09-21 15:07:15 +0300

    тесты все еще не работают

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,17 +2,28 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import Header from "./Header";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Компонент Header", () => {
   it("Должен отрендерить логотип", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
     const img = screen.getByAltText("logo");
 
     expect(img).toBeInTheDocument();
   });
 
   it("Успешный рендер Header", () => {
-    const component = renderer.create(<Header />).toJSON();
+    const component = renderer
+      .create(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      )
+      .toJSON();
     expect(component).toMatchSnapshot();
   });
 });
