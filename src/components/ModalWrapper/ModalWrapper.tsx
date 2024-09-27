eb09bc5cc28fd9e988933b343696aadb97805746
Nina Bobyleva <2847171@mail.ru>
2024-09-27 13:21:53 +0300

    Окна авторизации и регистрации закрываются по клику на серый фон

--- src/components/ModalWrapper/ModalWrapper.tsx
+++ src/components/ModalWrapper/ModalWrapper.tsx
@@ -1,9 +1,13 @@
 import { createPortal } from "react-dom";
+import { useNavigate } from "react-router-dom";
+import { path } from "../../paths";
 
 export function ModalWrapper({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
   
   return createPortal (
     <div className="w-full h-full bg-black bg-opacity-20 fixed z-20 left-[0px] top-[0px]">
+      <div onClick={() => navigate(path.HOME)} className="w-[100%] h-[100%]"></div>
       {children}
     </div>,
     document.getElementById('modal')!,
